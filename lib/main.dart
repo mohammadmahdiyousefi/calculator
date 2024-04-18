65633acf0f258149a7eef8a35258fa2fcf2f3808
Mohammad Mahdi Yousefi <mmye1481@gmail.com>
2024-04-18 18:47:45 +0330

    v1.4.3

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//------------------------------------------------------------------------------
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
